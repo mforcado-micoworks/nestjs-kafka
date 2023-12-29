610ac332e8b843f799ffecc601f5978586e4a33a
Marjo Forcado <m.forcado@micoworks.jp>
2023-12-29 17:50:49 +0800

    Add connection to microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Create Hybrid app that handles HTTP and Non-HTTP.
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
