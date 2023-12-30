6dd46c4c5c1b66394f7bd9249b54a99d3b570222
Marjo Forcado <m.forcado@micoworks.jp>
2023-12-30 15:31:05 +0800

    Add build script

--- apps/producer/src/main.ts
+++ apps/producer/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ProducerModule } from './producer.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ProducerModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    ProducerModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['kafka:9092'],
+        },
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
