4024ef6fca98d1cd59085b858186325065e1d401
Marjo Forcado <m.forcado@micoworks.jp>
2023-12-30 14:10:56 +0800

    Remove boilerplate

--- apps/producer/src/producer.module.ts
+++ apps/producer/src/producer.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ProducerController } from './producer.controller';
-import { ProducerService } from './producer.service';
 
 @Module({
   imports: [],
-  controllers: [ProducerController],
-  providers: [ProducerService],
 })
 export class ProducerModule {}
