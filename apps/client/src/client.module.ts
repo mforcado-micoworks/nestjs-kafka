7130343e37fc05852a03a53317fe81d875dcecf9
Marjo Forcado <m.forcado@micoworks.jp>
2024-01-04 17:50:19 +0800

    Remove boilerplate

--- apps/client/src/client.module.ts
+++ apps/client/src/client.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ClientController } from './client.controller';
-import { ClientService } from './client.service';
 import { EventsModule } from './resources/events/events.module';
 
 @Module({
   imports: [EventsModule],
-  controllers: [ClientController],
-  providers: [ClientService],
 })
 export class ClientModule {}
